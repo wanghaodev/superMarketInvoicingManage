ae83a6dbd57f9bc73a27fdb07b60d5901d79eea5
admin <admin@LAPTOP-HA92TB8B>
2017-08-10 01:22:51 +0800

    记录文档

---
+++ superMarketInvoicingManage/src/main/env/doc/数据库设计/20170810/goods_category_tb_date.sql
@@ -0,0 +1,4 @@
+INSERT INTO `goods_category_tb` VALUES (1, 'SP', '食品', '0', 1, 1, NULL, NULL, NULL, NULL, NULL, 1, 1, 0, 1, '1', '2017-8-9 23:43:40', 1, '1', '2017-8-9 23:43:40', NULL, NULL);
+INSERT INTO `goods_category_tb` VALUES (2, 'SS', '熟食', '1', 1, 2, NULL, NULL, NULL, NULL, NULL, 1, 1, 0, 1, '1', '2017-8-9 23:44:12', 1, '1', '2017-8-9 23:44:12', NULL, NULL);
+INSERT INTO `goods_category_tb` VALUES (3, 'SG', '水果', '1', 1, 2, NULL, NULL, NULL, NULL, NULL, 1, 1, 0, 1, '1', '2017-8-9 23:44:45', 1, '1', '2017-8-23 23:45:54', NULL, NULL);
+INSERT INTO `goods_category_tb` VALUES (4, 'SX', '生鲜', '1', 1, 2, NULL, NULL, NULL, NULL, NULL, 1, 1, 0, 1, '1', '2017-8-9 23:46:37', NULL, NULL, '2017-8-9 23:46:37', NULL, NULL);
